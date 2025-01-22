b8d6c7ec44e4955a5d432cb2617775672f22337e
talibytes <shaf.talia1@gmail.com>
2025-01-21 22:45:19 -0700

    Began work on GUI update

--- ClipHist.py
+++ ClipHist.py
@@ -16,13 +16,14 @@ maxClipboardItems = 20
 
 
 """  START OF PROGRAM  """
-import os                           # used to check if named pipe exists
+import os                           # used to check if named pipe exists, get assets for GUI
 from pyautogui import position      # open GUI next to mouse
 from pyautogui import hotkey        # simulate paste from system clipboard
 import pyperclip                    # used to manage system clipboard and listen for changes
 import threading                    # used to run multiple listeners concurrently
 from time import sleep              # reduces number of clipboard checks by waiting
 from tkinter import *               # used for copy history gui
+#from tkinter import Tk, Canvas, PhotoImage
 
 
 class tClipboardManager:
@@ -75,64 +76,153 @@ clipboard = tClipboardManager()
 
 
 def clipHistGUI():
+  #root = Tk()
+  #mouseX = position().x
+  #mouseY = position().y
+  #root.geometry(f"405x450+{mouseX - 202.5}+{mouseY - 225}")
+#
+  #root.title('ClipHist')
+  #root["background"] = "gray15"
+  #root.resizable(False, False)
+
+  #scrollbar = Scrollbar(root)
+  #scrollbar.pack(side=RIGHT, fill=Y)
+#
+  #historyList = Text(
+  #  root,
+  #  background="gray20",
+  #  foreground="gray80",
+  #  selectbackground="gray50",
+  #  selectforeground="white",
+  #  wrap="word",
+  #  yscrollcommand=scrollbar.set
+  #)
+#
+  #historyList.pack(side=LEFT, fill=BOTH, expand=True)
+  #scrollbar.config(command=historyList.yview)
+#
+  #for index, item in enumerate(clipboard.history):
+  #  tagName = f"item_{index}"
+  #  spacerTagName = f"{tagName}_spacer"
+  #  historyList.insert(END, f"{item}\n", tagName)
+#
+  #  historyList.tag_config(
+  #    tagName,
+  #    foreground="white",
+  #    background="gray30",
+  #    font=("Arial",12),
+  #    spacing1=5,
+  #    spacing3=5
+  #  )
+  #  historyList.tag_bind(tagName, "<Button-1>", lambda e, i=index: onSelect(i))
+#
+  #  historyList.insert(END, "\n", spacerTagName)
+  #  historyList.tag_config(
+  #    spacerTagName,
+  #    background="gray20"
+  #  )
+#
+#
+  #def onSelect(index):
+  #  # close ClipHist GUI
+  #  root.destroy()
+  #  
+  #  # bring to top of clipboard, put in system clipboard
+  #  clipboard.addToHistory(clipboard.history[index], True)
+  #  
+  #  # paste from system clipboard after small delay to resume focus on previous
+  #  hotkey('ctrl', 'v')
+#
+  #mainloop()
+
+
+  #GENERATED WITH TKINTER DESIGNER
+  assets_path = os.path.dirname(__file__)
+  assets_path = assets_path + '/assets/'
+  print(assets_path)
+
   root = Tk()
-  mouseX = position().x
-  mouseY = position().y
-  root.geometry(f"405x450+{mouseX}+{mouseY}")
+  mouseX = position().x - 202
+  mouseY = position().y - 225
+  root.geometry(f"405x450+{(mouseX)}+{(mouseY)}")
 
   root.title('ClipHist')
   root["background"] = "gray15"
   root.resizable(False, False)
 
-  scrollbar = Scrollbar(root)
-  scrollbar.pack(side=RIGHT, fill=Y)
-
-  historyList = Text(
+  canvas = Canvas(
     root,
-    background="gray20",
-    foreground="gray80",
-    selectbackground="gray50",
-    selectforeground="white",
-    wrap="word",
-    yscrollcommand=scrollbar.set
+    bg = "gray15",
+    height = 450,
+    width = 405,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+  )
+  canvas.place(x=0,y=0)
+
+  # BACKGROUND
+  canvas.create_rectangle(
+    0.0,
+    0.0,
+    405.0,
+    450.0,
+    fill="#1E1E1E",
+    outline=""
   )
 
-  historyList.pack(side=LEFT, fill=BOTH, expand=True)
-  scrollbar.config(command=historyList.yview)
 
-  for index, item in enumerate(clipboard.history):
-    tagName = f"item_{index}"
-    spacerTagName = f"{tagName}_spacer"
-    historyList.insert(END, f"{item}\n", tagName)
+  # LOOP  COPIED ITEMS (lower python program line number = lower z-index in rendered GUI)
 
-    historyList.tag_config(
-      tagName,
-      foreground="white",
-      background="gray30",
-      font=("Arial",12),
-      spacing1=5,
-      spacing3=5
-    )
-    historyList.tag_bind(tagName, "<Button-1>", lambda e, i=index: onSelect(i))
+  # SPACER RECTANGLE (BLUE)
+  canvas.create_rectangle(
+    0.0,
+    73.0,
+    405.0,
+    82.0,
+    fill = "#0000FF",
+    outline=""
+  )
 
-    historyList.insert(END, "\n", spacerTagName)
-    historyList.tag_config(
-      spacerTagName,
-      background="gray20"
-    )
+  # image for background of copied item, and image added to canvas
+  image_image_copiedItem = PhotoImage(file = assets_path + "rounded_button.png")
+  image_copiedItem = canvas.create_image(
+    202.0,
+    45.0,
+    image = image_image_copiedItem
+  )
+
+  canvas.create_text(
+    13.0,
+    14.0,
+    anchor="nw",
+    text="SomeTypedText",
+    fill="#FFFFFF",
+    font=("Inter", 12 * -1)
+  )
 
 
-  def onSelect(index):
-    # close ClipHist GUI
-    root.destroy()
-    
-    # bring to top of clipboard, put in system clipboard
-    clipboard.addToHistory(clipboard.history[index], True)
-    
-    # paste from system clipboard after small delay to resume focus on previous
-    hotkey('ctrl', 'v')
+  # BOTTOM BORDER RECTANGLE (RED)
+  canvas.create_rectangle(
+    0.0,
+    442.0,
+    405.0,
+    450.0,
+    fill = "#FF0000",
+    outline=""
+  )
 
-  mainloop()
+  # TOP BORDER RECTANGLE (RED)
+  canvas.create_rectangle(
+    0.0,
+    0.0, 
+    405.0,
+    9.0,
+    fill = "#FF0000",
+    outline=""
+  )
+
+  root.mainloop()
 
 
 
